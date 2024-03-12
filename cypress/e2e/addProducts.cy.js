0776fe211a118108fe5d86bb865734d06c358552
Kristian Vintse <124249938+krisvintse@users.noreply.github.com>
2024-03-12 17:07:02 +0200

    Delete cypress directory

--- cypress/e2e/addProducts.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Test cart functionality', () => {
-    it('Should add products to the cart', () => {
-      cy.AddProducts('standard_user', 'secret_sauce', '[data-test="add-to-cart-sauce-labs-backpack"]', '[data-test="add-to-cart-sauce-labs-bike-light"]');
-    })
-})
